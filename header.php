275445340deb161b1b991a29abcba0ac2a26c4ee
Isaiah-Simon <zays1993@gmail.com>
2014-04-30 14:48:36 -0400

    Finished header content

--- header.php
+++ header.php
@@ -1,6 +1,10 @@
 <header>
 	<img src="images\logo\logo-quick-computing.png" alt="Quick computing logo">
-	<p class="headEmail">Email: <a href="mailto:contact@quickcomputing.com">contact@quickcomputing.com</a></p>
+	<ul class="accountLinks">
+		<li><a href="login.php">Log in</a></li>
+		<li><a href="signUp">Sign up</a></li>
+	<ul>
+	<p class="headEmail"><span  class="bold">Email:</span> <a href="mailto:contact@quickcomputing.com">contact@quickcomputing.com</a></p>
 	<br>
-	<p class="headPhone">Phone: (123) 456-7890 </p>
+	<p class="headPhone" class="bold"><span  class="bold">Phone:</span> (123) 456-7890 </p>
 </header>
