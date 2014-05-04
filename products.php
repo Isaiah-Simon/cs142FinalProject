e9bc26d42149967db174e1b5bf88b5f2bfbaeb15
Isaiah-Simon <zays1993@gmail.com>
2014-05-03 21:08:54 -0400

    Added first and second products

--- products.php
+++ products.php
@@ -5,28 +5,55 @@
 			<? include ("nav.php"); ?>
 
 			<section id="content">
-				<aside id="localNav">
-					<ul>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/motherboards.php">Motherboards</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/graphicsCards.php">Graphics Cards</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/hardDrives.php">Hard Drives</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingKeyboards.php">Gaming Keyboards</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingMice.php">Gaming Mice</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/monitors.php">Monitors</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/memory.php">Memory</a></li>
-					</ul>
-				</aside>
+				<? include ("localNav.php"); ?>
 
 				<section class="mainContent">
-					
+
 					<section class="featuredItem">
+						<h1>Featured Items</h1>
+
+						<!----------------------- First Featured Item ---------------------------------->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingKeyboards.php">Gaming Keyboards</a></h2>
+							<h3>Logitech G710+
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">$129.99</a>
+								</span>
+
+								<span class="oldPrice">$149.99</span>
+
+								<span class="savedPercent">Save 13%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/gamingKeyboardCarousel.jpg" alt="G710+ Gaming Keyboard">
+							</a>
+						</section>
+
+						<!----------------------- Second Featured Item ---------------------------------->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingMice.php">Gaming Mice</a></h2>
+							<h3>Logitech G600 Gaming Mouse 
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG600Mouse.php">$14.99</a>
+								</span>
+
+								<span class="oldPrice">$69.99</span>
+
+								<span class="savedPercent">Save 79%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG600Mouse.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/gamingMouseCarousel.jpg" alt="G600 Gaming Mouse">
+							</a>
+						</section>
+
+
 					</section>
 
 					<section class="productsTable">
 						<table>
 					        <thead>
-					            <tr>
-					            </tr>
 					        </thead>
 
 					        <tbody>
