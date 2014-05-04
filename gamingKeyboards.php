6311a44cd3e5e5c4982d6563f5595c50beae3dff
Isaiah-Simon <zays1993@gmail.com>
2014-05-04 02:05:20 -0400

    Added the rest of the keyboards to the list

--- gamingKeyboards.php
+++ gamingKeyboards.php
@@ -42,7 +42,7 @@
 					                <!-- Price td -->	
 					                <td>
 					                	<p class="oldPrice">$149.99</p>
-					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$129.99</a>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">$129.99</a>
 					                	<p class="savedPercent">Save 13%</p>
 					                </td>
 					            </tr>
@@ -72,10 +72,101 @@
 					                <!-- Price td -->	
 					                <td>
 					                	<p class="oldPrice">$99.99</p>
-					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$69.99</a>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/ROCCATIskuFX.php">$69.99</a>
 					                	<p class="savedPercent">Save 30%</p>
 					                </td>
 					            </tr>
+
+					            <tr>
+					            	<!-- Image td -->
+					                <td>
+					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/DeathStalker.php">
+					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/DeathStalker/logo.jpg" alt="DeathStalker Logo">
+					                	</a>
+					                </td>
+
+					                <!-- Description td -->
+					                <td>
+					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/DeathStalker.php">
+					                			Razer DeathStalker Expert Gaming Keyboard</a></p>
+					                	<ul class="description">
+					                		<li><span class="bold">Mouse Included:</span> No</li>
+					                		<li><span class="bold">Palm Rest:</span> Integrated</li>
+					                		<li><span class="bold">Operating Systems Supported:</span> Windows 8/ Windows 7 / Windows Vista / Windows XP (32-bit) / Mac OS X (v10.6 to 10.8)</li>
+					                		<li><span class="bold">Normal Keys:</span> 125</li>
+											<li><span class="bold">Model Number:</span> RZ03-00800100-R3U1</li>
+											<li><span class="bold">Return Policy:</span> 30-Day Refund</li>
+					                	</ul>
+					                </td>
+					                
+					                <!-- Price td -->	
+					                <td>
+					                	<p class="oldPrice">$79.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/DeathStalker.php">$68.99</a>
+					                	<p class="savedPercent">Save 14%</p>
+					                </td>
+					            </tr>
+
+					            <tr>
+					            	<!-- Image td -->
+					                <td>
+					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/K70.php">
+					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/K70/logo.jpg" alt="Corsair K70 Logo">
+					                	</a>
+					                </td>
+
+					                <!-- Description td -->
+					                <td>
+					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/K70.php">
+					                			Corsair Vengeance K70 Mechanical Gaming Keyboard Black - Cherry MX Red</a></p>
+					                	<ul class="description">
+					                		<li><span class="bold">Mouse Included:</span> No</li>
+					                		<li><span class="bold">Palm Rest:</span> Detachable</li>
+					                		<li><span class="bold">Operating Systems Supported:</span> Windows 7, Windows 8, Windows Vista, or Windows XP</li>
+					                		<li><span class="bold">System Requirement:</span> PC with high power USB 2.0 port (+500mA) At least 35MB of hard disk space Two USB connectors are required for USB pass-through</li>
+											<li><span class="bold">Model Number:</span> CH-9000011-NA</li>
+											<li><span class="bold">Return Policy:</span> 30-Day Refund</li>
+					                	</ul>
+					                </td>
+					                
+					                <!-- Price td -->	
+					                <td>
+					                	<p class="oldPrice">$129.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/K70.php">$119.99</a>
+					                	<p class="savedPercent">Save 8%</p>
+					                </td>
+					            </tr>
+
+					            <tr>
+					            	<!-- Image td -->
+					                <td>
+					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/QuickFire.php">
+					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/QuickFire/logo.jpg" alt="CM Storm QuickFire Logo">
+					                	</a>
+					                </td>
+
+					                <!-- Description td -->
+					                <td>
+					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/QuickFire.php">
+					                			CM Storm QuickFire Rapid - Compact Mechanical Gaming Keyboard with CHERRY MX Blue Switches</a></p>
+					                	<ul class="description">
+					                		<li><span class="bold">Mouse Included:</span> No</li>
+					                		<li><span class="bold">Palm Rest:</span> No</li>
+					                		<li><span class="bold">Dimensions:</span> 14" (L) x 5" (W) x 1" (H)</li>
+					                		<li><span class="bold">Mechanical Keyboard:</span> Yes</li>
+											<li><span class="bold">Model Number:</span> SGK-4000-GKCL1-US</li>
+											<li><span class="bold">Return Policy:</span> 30-Day Refund</li>
+					                	</ul>
+					                </td>
+					                
+					                <!-- Price td -->	
+					                <td>
+					                	<p class="oldPrice">$89.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/QuickFire.php">$79.99</a>
+					                	<p class="savedPercent">Save 11%</p>
+					                </td>
+					            </tr>
+
 					        <tbody>
 					    </table>
 					</section>
