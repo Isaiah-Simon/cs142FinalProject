bf87c89db3cde34255238d7f498807250c7b221b
Isaiah-Simon <zays1993@gmail.com>
2014-05-03 21:08:09 -0400

    added LocalNav file

---
+++ localNav.php
@@ -0,0 +1,12 @@
+				<aside id="localNav">
+					<ul>
+											
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingKeyboards.php">Gaming Keyboards</a></li>
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingMice.php">Gaming Mice</a></li>
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/graphicsCards.php">Graphics Cards</a></li>	
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/hardDrives.php">Hard Drives</a></li>
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/memory.php">Memory</a></li>
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/monitors.php">Monitors</a></li>
+						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/motherboards.php">Motherboards</a></li>	
+					</ul>
+				</aside>
