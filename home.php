36633fc45fdb2655a4e1d8d9d6a9515da6eacbb5
Isaiah-Simon <zays1993@gmail.com>
2014-05-03 21:08:34 -0400

    Removed hard coded nav and changed to include

--- home.php
+++ home.php
@@ -5,17 +5,7 @@
 			<? include ("nav.php"); ?>
 
 			<section id="content">
-				<aside id="localNav">
-					<ul>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/motherboards.php">Motherboards</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/graphicsCards.php">Graphics Cards</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/hardDrives.php">Hard Drives</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingKeyboards.php">Gaming Keyboards</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingMice.php">Gaming Mice</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/monitors.php">Monitors</a></li>
-						<li><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/memory.php">Memory</a></li>
-					</ul>
-				</aside>
+				<? include ("localNav.php"); ?>
 
 				<section class="mainContent">
 					<!--
