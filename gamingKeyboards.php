51efb2a19f49394bd02589e1d9edd6a3d7af4e72
Isaiah-Simon <zays1993@gmail.com>
2014-05-04 01:46:54 -0400

    Added second item to the list

--- gamingKeyboards.php
+++ gamingKeyboards.php
@@ -41,39 +41,39 @@
 					                
 					                <!-- Price td -->	
 					                <td>
-					                	<p class="oldPrice">$249.99</p>
-					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$189.99</a>
-					                	<p class="savedPercent">Save 24%</p>
+					                	<p class="oldPrice">$149.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$129.99</a>
+					                	<p class="savedPercent">Save 13%</p>
 					                </td>
 					            </tr>
 
 					            <tr>
 					            	<!-- Image td -->
 					                <td>
-					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
-					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/G710Plus/logo.jpg" alt="G710 Plus Logo">
+					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/ROCCATIskuFX.php">
+					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/ROCCATIskuFX/logo.jpg" alt="ROCCATIskuFX Logo">
 					                	</a>
 					                </td>
 
 					                <!-- Description td -->
 					                <td>
-					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
-					                			Logitech G710+ USB Wired Gaming Mechanical Keyboard</a></p>
+					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/ROCCATIskuFX.php">
+					                			ROCCAT Isku FX ROC-12-901 Black USB Wired Gaming Multicolor Keyboard</a></p>
 					                	<ul class="description">
 					                		<li><span class="bold">Mouse Included:</span> No</li>
-					                		<li><span class="bold">Palm Rest:</span> Detachable</li>
-					                		<li><span class="bold">Normal Keys:</span> 110</li>
-					                		<li><span class="bold">System Requirements:</span> Windows 8, Windows 7, or Windows Vista Two USB ports 70 MB of available hard disk space Internet connection for optional software download</li>
-											<li><span class="bold">Model Number:</span> 920-003887</li>
+					                		<li><span class="bold">Palm Rest:</span> Yes</li>
+					                		<li><span class="bold">Normal Keys:</span> 123</li>
+					                		<li><span class="bold">Operating Systems Supported:</span> Windows XP, Windows Vista 32/64-bit, Windows 7 32/64-bit Windows 8 / Windows 8 Pro</li>
+											<li><span class="bold">Model Number:</span> ROC-12-901</li>
 											<li><span class="bold">Return Policy:</span> 30-Day Refund</li>
 					                	</ul>
 					                </td>
 					                
 					                <!-- Price td -->	
 					                <td>
-					                	<p class="oldPrice">$249.99</p>
-					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$189.99</a>
-					                	<p class="savedPercent">Save 24%</p>
+					                	<p class="oldPrice">$99.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$69.99</a>
+					                	<p class="savedPercent">Save 30%</p>
 					                </td>
 					            </tr>
 					        <tbody>
