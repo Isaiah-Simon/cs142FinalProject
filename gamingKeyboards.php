723f6b48abf2014bc4a12678d9c42d59dd976ff9
Isaiah-Simon <zays1993@gmail.com>
2014-05-04 00:42:13 -0400

    Created initial gamingKeyboards page

---
+++ gamingKeyboards.php
@@ -0,0 +1,24 @@
+<? include ("top.php"); ?>
+	<body id="products">
+		<section id="container">
+			<? include ("header.php"); ?>
+			<? include ("nav.php"); ?>
+
+			<section id="content">
+				<? include ("localNav.php"); ?>
+
+				<section class="mainContent">
+					<section id="breadCrumbs">
+						<p><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/products.php">Products</a> >  Gaming Keyboards</p>
+					</section>
+
+					<section class="featuredItem">
+						<h1>Products</h1>
+						</section>
+					</section>
+				</section>
+			</section>
+			<? include ("footer.php"); ?>
+		</section>
+	</body>
+</html>
