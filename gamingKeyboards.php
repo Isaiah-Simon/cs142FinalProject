214168ba934184a49ec50b36370f5172499e3c59
Isaiah-Simon <zays1993@gmail.com>
2014-05-04 01:38:45 -0400

    Added and styled products for the list

--- gamingKeyboards.php
+++ gamingKeyboards.php
@@ -1,5 +1,5 @@
 <? include ("top.php"); ?>
-	<body id="products">
+	<body id="gamingKeyboards">
 		<section id="container">
 			<? include ("header.php"); ?>
 			<? include ("nav.php"); ?>
@@ -12,9 +12,72 @@
 						<p><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/products.php">Products</a> >  Gaming Keyboards</p>
 					</section>
 
-					<section class="featuredItem">
-						<h1>Products</h1>
-						</section>
+					<h1>Gaming Keyboards</h1>
+
+						<section class="itemList">
+						<table>
+					        <tbody>
+					            <tr>
+					            	<!-- Image td -->
+					                <td>
+					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
+					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/G710Plus/logo.jpg" alt="G710 Plus Logo">
+					                	</a>
+					                </td>
+
+					                <!-- Description td -->
+					                <td>
+					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
+					                			Logitech G710+ USB Wired Gaming Mechanical Keyboard</a></p>
+					                	<ul class="description">
+					                		<li><span class="bold">Mouse Included:</span> No</li>
+					                		<li><span class="bold">Palm Rest:</span> Detachable</li>
+					                		<li><span class="bold">Normal Keys:</span> 110</li>
+					                		<li><span class="bold">System Requirements:</span> Windows 8, Windows 7, or Windows Vista Two USB ports 70 MB of available hard disk space Internet connection for optional software download</li>
+											<li><span class="bold">Model Number:</span> 920-003887</li>
+											<li><span class="bold">Return Policy:</span> 30-Day Refund</li>
+					                	</ul>
+					                </td>
+					                
+					                <!-- Price td -->	
+					                <td>
+					                	<p class="oldPrice">$249.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$189.99</a>
+					                	<p class="savedPercent">Save 24%</p>
+					                </td>
+					            </tr>
+
+					            <tr>
+					            	<!-- Image td -->
+					                <td>
+					                	<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
+					                		<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/products/G710Plus/logo.jpg" alt="G710 Plus Logo">
+					                	</a>
+					                </td>
+
+					                <!-- Description td -->
+					                <td>
+					                	<p class="productListTitle"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">
+					                			Logitech G710+ USB Wired Gaming Mechanical Keyboard</a></p>
+					                	<ul class="description">
+					                		<li><span class="bold">Mouse Included:</span> No</li>
+					                		<li><span class="bold">Palm Rest:</span> Detachable</li>
+					                		<li><span class="bold">Normal Keys:</span> 110</li>
+					                		<li><span class="bold">System Requirements:</span> Windows 8, Windows 7, or Windows Vista Two USB ports 70 MB of available hard disk space Internet connection for optional software download</li>
+											<li><span class="bold">Model Number:</span> 920-003887</li>
+											<li><span class="bold">Return Policy:</span> 30-Day Refund</li>
+					                	</ul>
+					                </td>
+					                
+					                <!-- Price td -->	
+					                <td>
+					                	<p class="oldPrice">$249.99</p>
+					                	<p class="newPrice"><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$189.99</a>
+					                	<p class="savedPercent">Save 24%</p>
+					                </td>
+					            </tr>
+					        <tbody>
+					    </table>
 					</section>
 				</section>
 			</section>
