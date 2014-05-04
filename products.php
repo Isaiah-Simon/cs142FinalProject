ad6d25f2bbd03dbf0be7b21e6b0b09c44a28d3c9
Isaiah-Simon <zays1993@gmail.com>
2014-05-03 22:22:44 -0400

    Added featured products and images

--- products.php
+++ products.php
@@ -15,10 +15,10 @@
 					<section class="featuredItem">
 						<h1>Products</h1>
 
-						<!----------------------- First Featured Item ---------------------------------->
+						<!--- First Featured Item -->
 						<section>
 							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingKeyboards.php">Gaming Keyboards</a></h2>
-							<h3>Featured: Logitech G710+
+							<h3>Featured: Logitech G710+ Gaming Keyboard
 								<span class="newPrice">
 									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG710Plus.php">$129.99</a>
 								</span>
@@ -33,7 +33,7 @@
 							</a>
 						</section>
 
-						<!----------------------- Second Featured Item ---------------------------------->
+						<!--- Second Featured Item -->
 						<section>
 							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/gamingMice.php">Gaming Mice</a></h2>
 							<h3>Featured: Logitech G600 Gaming Mouse 
@@ -48,6 +48,97 @@
 							
 							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/logitechG600Mouse.php">
 								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/gamingMouseCarousel.jpg" alt="G600 Gaming Mouse">
+							</a>
+						</section>
+
+						<!-- Third Featured Item -->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/graphicsCards.php">Graphics Cards</a></h2>
+							<h3>Featured: MSI Radeon R9 290x 4GB
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/msiRadeonR9290x.php">$599.99</a>
+								</span>
+
+								<span class="oldPrice">$649.99</span>
+
+								<span class="savedPercent">Save 8%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/msiRadeonR9290x.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/graphicsCardCarousel.png" alt="MSI Radeon R9 290x 4GB">
+							</a>
+						</section>
+
+
+						<!--- Fourth Featured Item -->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/graphicsCards.php">Hard Drives</a></h2>
+							<h3>Featured: Samsung 840 Pro Series 2.5-Inch 256 GB SSD
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">$199.99</a>
+								</span>
+
+								<span class="oldPrice">$269.99</span>
+
+								<span class="savedPercent">Save 26%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/SAMSUNG840ProSeries256GBSSD.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/hardDriveCarousel.jpg" alt="Samsung 840 Pro Series 2.5-Inch 256 GB SSD">
+							</a>
+						</section>
+
+						<!-- Fifth Featured Item -->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/memory.php">Memory</a></h2>
+							<h3>Featured: Corsair Vengeance Blue 8GB (2x4GB) DDR3 1600 MHz 
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/corsairVengeance8GB.php">$92.99</a>
+								</span>
+
+								<span class="oldPrice">$99.99</span>
+
+								<span class="savedPercent">Save 5%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/corsairVengeance8GB.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/memoryCarousel.jpg" alt="Corsair Vengeance Blue 8GB (2x4GB) DDR3 1600 MHz">
+							</a>
+						</section>
+
+						<!-- Sixth Featured Item -->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/monitors.php">Monitors</a></h2>
+							<h3>Featured: ASUS 27-Inch WQHD LED Professional Graphics Monitor
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/asusWQHDMonitor.php">$489.99</a>
+								</span>
+
+								<span class="oldPrice">$709.99</span>
+
+								<span class="savedPercent">Save 31%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/asusWQHDMonitor.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/monitorCarousel.jpg" alt="ASUS 27-Inch WQHD LED Professional Graphics Monitor">
+							</a>
+						</section>
+
+						<!-- Seventh Featured Item -->
+						<section>
+							<h2><a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/motherboards.php">Motherboards</a></h2>
+							<h3>Featured: GIGABYTE GA-Z87X-UD3H LGA 1150 Intel Motherboard
+								<span class="newPrice">
+									<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/GIGABYTEGA-Z87X-UD3H.php">$139.99</a>
+								</span>
+
+								<span class="oldPrice">$164.99</span>
+
+								<span class="savedPercent">Save 15%</span>
+							</h3>
+							
+							<a href="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/GIGABYTEGA-Z87X-UD3H.php">
+								<img src="http://www.uvm.edu/~isimon/cs142/cs142FinalProject/images/carousel/motherboardCarousel.jpg" alt="GIGABYTE GA-Z87X-UD3H LGA 1150 Intel Motherboard">
 							</a>
 						</section>
 
